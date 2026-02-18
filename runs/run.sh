c1c849cc5fa6c752002a650b0e2a96cffb3b6531
Anton Pembek <98619025+Antondfger@users.noreply.github.com>
2026-02-18 11:50:56 +0300

    Update run.sh

--- runs/run.sh
+++ runs/run.sh
@@ -8,7 +8,7 @@ python dl.py datasets_info=Tmall dataset_params.num_negatives=50000 model_params
 python dl.py datasets_info=Tmall dataset_params.num_negatives=50000 model_params.num_blocks=3 model_params.num_heads=4 model_params.dropout_rate=0.1 model_params.hidden_units=256 type_loss=ce temp=1 seqrec_module.type_adj=logit adj=0.4 random_state=17,42,52 --multirun
 
 # Margin Adjustment on Tmall dataset
-python dl.py datasets_info=Tmall dataset_params.num_negatives=50000 model_params.num_blocks=3 model_params.num_heads=4 model_params.dropout_rate=0.1 model_params.hidden_units=256 type_loss=ce temp=1 seqrec_module.type_adj=margin adj=0.01 random_state=17,42,52 --multirun
+python dl.py datasets_info=Tmall dataset_params.num_negatives=50000 model_params.num_blocks=3 model_params.num_heads=4 model_params.dropout_rate=0.1 model_params.hidden_units=256 type_loss=ce temp=1 seqrec_module.type_adj=margin adj=0.01 counts=True random_state=17,42,52 --multirun
 
 # Cross-Entropy on Tmall dataset
 python dl.py datasets_info=Tmall dataset_params.num_negatives=50000 model_params.num_blocks=3 model_params.num_heads=4 model_params.dropout_rate=0.1 model_params.hidden_units=256 type_loss=ce temp=1 random_state=17,42,52 --multirun
@@ -24,7 +24,7 @@ python dl.py datasets_info=Zvuk model_params.num_blocks=1 model_params.num_heads
 python dl.py datasets_info=Zvuk model_params.num_blocks=1 model_params.num_heads=2 model_params.dropout_rate=0.5 model_params.hidden_units=192 type_loss=ce temp=1 seqrec_module.type_adj=logit adj=0.8 random_state=17,42,52 --multirun
 
 # Margin Adjustment on Zvuk dataset
-python dl.py datasets_info=Zvuk model_params.num_blocks=1 model_params.num_heads=2 model_params.dropout_rate=0.5 model_params.hidden_units=192 type_loss=ce temp=1 seqrec_module.type_adj=margin adj=10 random_state=17,42,52 --multirun
+python dl.py datasets_info=Zvuk model_params.num_blocks=1 model_params.num_heads=2 model_params.dropout_rate=0.5 model_params.hidden_units=192 type_loss=ce temp=1 seqrec_module.type_adj=margin counts=True adj=10 random_state=17,42,52 --multirun
 
 # Cross-Entropy on Zvuk dataset
 python dl.py datasets_info=Zvuk model_params.num_blocks=1 model_params.num_heads=2 model_params.dropout_rate=0.5 model_params.hidden_units=192 type_loss=ce temp=1 random_state=17,42,52 --multirun
@@ -40,7 +40,7 @@ python dl.py datasets_info=Yambda model_params.num_blocks=1 model_params.num_hea
 python dl.py datasets_info=Yambda model_params.num_blocks=2 model_params.num_heads=2 model_params.dropout_rate=0.2 model_params.hidden_units=256 type_loss=ce temp=1 seqrec_module.type_adj=logit adj=0.6 random_state=17,42,52 --multirun
 
 # Margin Adjustment on Yambda dataset
-python dl.py datasets_info=Yambda model_params.num_blocks=2 model_params.num_heads=2 model_params.dropout_rate=0.2 model_params.hidden_units=256 type_loss=ce temp=1 seqrec_module.type_adj=margin adj=5 random_state=17,42,52 --multirun
+python dl.py datasets_info=Yambda model_params.num_blocks=2 model_params.num_heads=2 model_params.dropout_rate=0.2 model_params.hidden_units=256 type_loss=ce temp=1 seqrec_module.type_adj=margin counts=True adj=5 random_state=17,42,52 --multirun
 
 # Cross-Entropy on Yambda dataset
 python dl.py datasets_info=Yambda model_params.num_blocks=2 model_params.num_heads=2 model_params.dropout_rate=0.2 model_params.hidden_units=256 type_loss=ce temp=1 random_state=17,42,52 --multirun
@@ -56,7 +56,7 @@ python dl.py datasets_info=Yoochoose model_params.num_blocks=3 model_params.num_
 python dl.py datasets_info=Yoochoose model_params.num_blocks=2 model_params.num_heads=1 model_params.dropout_rate=0.3 model_params.hidden_units=128 type_loss=ce temp=1 seqrec_module.type_adj=logit adj=0.9 random_state=17,42,52 --multirun
 
 # Margin Adjustment on Yoochoose dataset
-python dl.py datasets_info=Yoochoose model_params.num_blocks=2 model_params.num_heads=1 model_params.dropout_rate=0.3 model_params.hidden_units=128 type_loss=ce temp=1 seqrec_module.type_adj=margin adj=15 random_state=17,42,52 --multirun
+python dl.py datasets_info=Yoochoose model_params.num_blocks=2 model_params.num_heads=1 model_params.dropout_rate=0.3 model_params.hidden_units=128 type_loss=ce temp=1 seqrec_module.type_adj=margin counts=True adj=15 random_state=17,42,52 --multirun
 
 # Cross-Entropy on Yoochoose dataset
 python dl.py datasets_info=Yoochoose model_params.num_blocks=2 model_params.num_heads=1 model_params.dropout_rate=0.3 model_params.hidden_units=128 type_loss=ce temp=1 random_state=17,42,52 --multirun
